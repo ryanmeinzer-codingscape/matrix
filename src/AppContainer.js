f17172882c466984a447ca356600e0fd6eee4016
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-05-19 17:51:24 -0700

    removed column menus and replaced with entire table toolbar with export function enabled (but not refined)

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,15 +1,16 @@
-import {DataGrid} from '@material-ui/data-grid'
+import {DataGrid, GridToolbar} from '@material-ui/data-grid'
 import Tooltip from '@material-ui/core/Tooltip'
 
 const columns = [
     {
         field: 'id',
-        hide: '☑',
+        hide: true,
     },
     {
         field: 'valueTitle',
         headerName: ' ',
-        sortable: '☐',
+        sortable: false,
+        disableColumnSelector: true,
         flex: 1,
         renderCell: (params) => (
             <Tooltip title={params.row.valueTitleDescription}>
@@ -20,6 +21,7 @@ const columns = [
     {
         field: 'internalLabs',
         headerName: 'Internal Labs $$$$$',
+        sortable: false,
         flex: 1,
         description: "Internal Technology Labs come with the cost of staffing an entire engineering and design team in house outside of your normal engineering internal organization.",
         renderCell: (params) => (
@@ -31,6 +33,7 @@ const columns = [
     {
         field: 'largeConsultancies',
         headerName: 'Large Consultancies $$$$',
+        sortable: false,
         flex: 1,
         description: "International Consultancies that have minimum contract sizes for multi year duration contracts. Quality can be high but price can be out of reach for most customers.",
         renderCell: (params) => (
@@ -42,6 +45,7 @@ const columns = [
     {
         field: 'codingscape',
         headerName: 'Codingscape $$$',
+        sortable: false,
         flex: 1,
         description: "Codingscape is the right balance of high-quality software development with the agility to meet tight deadlines at the market rate price to work with Senior American Software Engineers.",
         renderCell: (params) => (
@@ -53,6 +57,7 @@ const columns = [
     {
         field: 'nearshoreFirms',
         headerName: 'Nearshore Firms $$',
+        sortable: false,
         flex: 1,
         description: "Nearshore Firms specialize in having American Sales people that then repackage contracts for foreign developers. Communication can be better than offshore firms but often quality still suffers with language and timezone barriers.",
         renderCell: (params) => (
@@ -64,6 +69,7 @@ const columns = [
     {
         field: 'offshoreFirms',
         headerName: 'Offshore Firms $',
+        sortable: false,
         flex: 1,
         description: "Offshore Firms are cheap but their benefits end there. Quality, communication, and meeting deadlines within originally quoted budgets are all problems with Offshore Firms.",
         renderCell: (params) => (
@@ -231,7 +237,16 @@ const AppContainer = () => {
     return (
         <div style={{display: 'flex', height: '100vh'}}>
             <div style={{flexGrow: 1}}>
-                <DataGrid rows={rows} columns={columns} pageSize={10} checkboxSelection />
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={10}
+                    checkboxSelection
+                    disableColumnMenu={true}
+                    components={{
+                    Toolbar: GridToolbar,
+                    }}
+                />
             </div>
         </div>
     )
