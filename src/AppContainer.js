89d562bf13db059e0235236c8a482551a9262e92
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-07 18:09:14 -0700

    added black background and zoom effect for tooltips

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -6,6 +6,7 @@ import {
     GridToolbarExport
 } from '@material-ui/data-grid'
 import Tooltip from '@material-ui/core/Tooltip'
+import Zoom from '@material-ui/core/Zoom';
 import {withStyles, makeStyles, createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import LoRes from './fonts/lores12ot-reg-webfont.woff2'
@@ -33,7 +34,7 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles({
     root: {
-        // background: '#000000',
+        background: '#000000',
         border: 0,
         color: '#FF4844'
     }
@@ -60,7 +61,7 @@ const columns = [
         disableColumnSelector: true,
         flex: 1.4,
         renderCell: (params) => (
-            <LightTooltip className={'root'} title={params.row.valueTitleDescription}>
+            <LightTooltip className={'root'} title={params.row.valueTitleDescription} TransitionComponent={Zoom}>
                 <span>{params.value}</span>
             </LightTooltip>
         )
@@ -73,7 +74,7 @@ const columns = [
         description: "Internal Technology Labs come with the cost of staffing an entire engineering and design team in house outside of your normal engineering internal organization.",
         type: 'boolean',
         renderCell: (params) => (
-            <LightTooltip title={params.row.internalLabsDescription}>
+            <LightTooltip title={params.row.internalLabsDescription} TransitionComponent={Zoom}>
                 <span>{params.value ? '✓' : '◯'}</span>
             </LightTooltip>
         )
@@ -86,7 +87,7 @@ const columns = [
         description: "International Consultancies that have minimum contract sizes for multi year duration contracts. Quality can be high but price can be out of reach for most customers.",
         type: 'boolean',
         renderCell: (params) => (
-            <LightTooltip title={params.row.largeConsultanciesDescription}>
+            <LightTooltip title={params.row.largeConsultanciesDescription} TransitionComponent={Zoom}>
                 <span>{params.value ? '✓' : '◯'}</span>
             </LightTooltip>
         )
@@ -99,7 +100,7 @@ const columns = [
         description: "Codingscape is the right balance of high-quality software development with the agility to meet tight deadlines at the market rate price to work with Senior American Software Engineers.",
         type: 'boolean',
         renderCell: (params) => (
-            <LightTooltip title={params.row.codingscapeDescription}>
+            <LightTooltip title={params.row.codingscapeDescription} TransitionComponent={Zoom}>
                 <span>{params.value ? '✓' : '◯'}</span>
             </LightTooltip>
         )
@@ -112,7 +113,7 @@ const columns = [
         description: "Nearshore Firms specialize in having American Sales people that then repackage contracts for foreign developers. Communication can be better than offshore firms but often quality still suffers with language and timezone barriers.",
         type: 'boolean',
         renderCell: (params) => (
-            <LightTooltip title={params.row.nearshoreFirmsDescription}>
+            <LightTooltip title={params.row.nearshoreFirmsDescription} TransitionComponent={Zoom}>
                 <span>{params.value ? '✓' : '◯'}</span>
             </LightTooltip>
         )
@@ -125,7 +126,7 @@ const columns = [
         description: "Offshore Firms are cheap but their benefits end there. Quality, communication, and meeting deadlines within originally quoted budgets are all problems with Offshore Firms.",
         type: 'boolean',
         renderCell: (params) => (
-            <LightTooltip title={params.row.offshoreFirmsDescription}>
+            <LightTooltip title={params.row.offshoreFirmsDescription} TransitionComponent={Zoom}>
                 <span>{params.value ? '✓' : '◯'}</span>
             </LightTooltip>
         )
