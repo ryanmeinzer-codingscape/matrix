a2d1458fc4213ca7c899fba52a62d314169ef006
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2021-06-15 17:28:33 -0700

    adjusted margin for mobile

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -130,7 +130,7 @@ const AppContainer = () => {
             },
             '& .MuiDataGrid-cell': {
                 border: 0,
-                padding: nonMobile ? 16 : 8
+                padding: nonMobile ? 16 : 4
             },
             '& .MuiDataGrid-iconSeparator': {
                 color: isDarkTheme ? '#ffffff' : '#2B2424'
